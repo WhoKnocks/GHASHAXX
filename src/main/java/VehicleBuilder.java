a692c04b7b3c954fa4c9deed7a69e66b6efa565c
mgreefs <michael.greefs@hotmail.com>
2018-03-01 19:34:21 +0100

    vehicles

---
+++ src/main/java/VehicleBuilder.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class VehicleBuilder {
+    public static List<Vehicle> buildVehicles(int numberOfVerhicles) {
+        return IntStream.range(0, numberOfVerhicles).mapToObj(value -> new Vehicle()).collect(Collectors.toList());
+    }
+}
