e650d9a33b40784ff6c60ec08ef49fc7a1d0af0f
mgreefs <michael.greefs@hotmail.com>
2018-03-01 19:24:36 +0100

    rides

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,28 @@ public class Main {
     public static void main(String[] args) {
         List<List<Integer>> lines = IOUtil.getLines("test.in", " ", Integer::parseInt);
 
-        List<String> result = lines.stream()
-                .map(list -> list.stream()
-                        .map(Object::toString)
-                        .collect(Collectors.joining(" ")))
+        List<Integer> firstRow = lines.get(0);
+
+        int numberOfRows_R = firstRow.get(0);
+        int numberOfColumns_C = firstRow.get(1);
+        int numberVehicles_F = firstRow.get(2);
+        int numberOfRides_N = firstRow.get(3);
+        int perRideBonus_B = firstRow.get(4);
+        int numberOfSteps_T = firstRow.get(5);
+
+        List<Ride> rides = lines.subList(1, lines.size() - 1).stream()
+                .map(row -> new Ride(row.get(0), row.get(1), row.get(2), row.get(3), row.get(4), row.get(5)))
                 .collect(toList());
 
-        result.forEach(System.out::println);
 
-        IOUtil.writeLines("test.out", result);
+//        List<String> result = lines.stream()
+//                .map(list -> list.stream()
+//                        .map(Object::toString)
+//                        .collect(Collectors.joining(" ")))
+//                .collect(toList());
+//
+//        result.forEach(System.out::println);
+//
+//        IOUtil.writeLines("test.out", result);
     }
 }
---
+++ src/main/java/Ride.java
@@ -0,0 +1,65 @@
+public class Ride {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    private int earliestStart;
+    private int lastestFinish;
+
+    public Ride(int x1, int y1, int x2, int y2, int earliestStart, int lastestFinish) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.earliestStart = earliestStart;
+        this.lastestFinish = lastestFinish;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public int getEarliestStart() {
+        return earliestStart;
+    }
+
+    public void setEarliestStart(int earliestStart) {
+        this.earliestStart = earliestStart;
+    }
+
+    public int getLastestFinish() {
+        return lastestFinish;
+    }
+
+    public void setLastestFinish(int lastestFinish) {
+        this.lastestFinish = lastestFinish;
+    }
+}
